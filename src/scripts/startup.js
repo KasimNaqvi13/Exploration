a98f987f20076827c89b6d8bdf0904b9ccaa7302
naqvi.a <naqvi.a@cetastech.com>
2025-08-19 11:31:01 +0530

    deleted files

--- src/scripts/startup.js
+++
@@ -1,7 +0,0 @@
-Microsoft.Dynamics.NAV.InvokeExtensibilityMethod("OnCodeLoaded", []);
-
-document.body.innerHTML = `
-<div id="al-code-container">
-    <pre><code id="al-code-block" class="language-al"></code></pre>
-</div>
-`;
